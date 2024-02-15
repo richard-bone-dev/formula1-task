d8e4dcdf5fbd263a2d2d0a8f50b1e5cc43f93cf5
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2024-02-15 01:45:49 +0000

    Edited .csproj file and converted to Program.Main for easier reading

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,35 +1,41 @@
 ﻿using System.Net.Http.Json;
 
-List<string> weatherStationUris = new() {
-    @"weatherstation-1",
-    @"weatherstation-2",
-    @"weatherstation-3"
-};
-
-var weatherStationTasks = weatherStationUris.Select(GetTemperature);
-
-double temperature = 0;
-
-while (temperature == 0)
+internal class Program
 {
-    temperature = weatherStationTasks.Where(x => x.IsCompleted)
-        .Select(x => x.Result)
-        .First();
+    private static void Main(string[] args)
+    {
+        List<string> weatherStationUris = new() {
+            @"weatherstation-1",
+            @"weatherstation-2",
+            @"weatherstation-3"
+        };
+
+        var weatherStationTasks = weatherStationUris.Select(GetTemperature);
+
+        double temperature = 0;
+
+        while (temperature == 0)
+        {
+            temperature = weatherStationTasks.Where(x => x.IsCompleted)
+                .Select(x => x.Result)
+                .First();
+        }
+
+        Console.WriteLine($"The temperature {(temperature == 0 ? "could not be retrieved" : $"is {temperature:2}")}");
+
+        static Task<double> GetTemperature(string uri)
+        {
+            HttpClient client = new();
+
+            var httpResponse = client.GetAsync(uri).Result;
+            httpResponse.EnsureSuccessStatusCode();
+
+            var weatherModel = httpResponse.Content.ReadFromJsonAsync<WeatherTransformModel>().Result;
+
+            return Task.FromResult(weatherModel.Temperature);
+        };
+    }
 }
-
-Console.WriteLine($"The temperature {(temperature == 0 ? "could not be retrieved" : $"is {temperature:2}")}");
-
-static Task<double> GetTemperature(string uri)
-{
-    HttpClient client = new();
-
-    var httpResponse = client.GetAsync(uri).Result;
-    httpResponse.EnsureSuccessStatusCode();
-
-    var weatherModel = httpResponse.Content.ReadFromJsonAsync<WeatherTransformModel>().Result;
-
-    return Task.FromResult(weatherModel.Temperature);
-};
 
 record WeatherTransformModel(double Temperature);
 
